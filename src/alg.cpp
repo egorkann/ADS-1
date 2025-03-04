892f00348d8bc065f7084ec26d9024c564dbdd81
egorkann <minicraftnn@mail.ru>
2025-03-04 16:56:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,66 +1,63 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-    if (value <= 1)
-        return false;
-    if (value <= 3)
-        return true;
-    if (value % 2 == 0 || value % 3 == 0)
-        return false;
-    for (uint64_t i = 5; i * i <= value; i += 6) {
-        if (value % i == 0 || value % (i + 2) == 0)
-            return false;
-    }
+  if (value <= 1)
+    return false;
+  if (value <= 3)
     return true;
+  if (value % 2 == 0 || value % 3 == 0)
+    return false;
+  for (uint64_t i = 5; i * i <= value; i += 6) {
+    if (value % i == 0 || value % (i + 2) == 0)
+      return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    return 2;
-    uint64_t count = 0;
-    uint64_t num = 2;
-    while (count < n) {
-        if (checkPrime(num)) {
-            count++;
-        }
-        num++;
+  uint64_t count = 0;
+  uint64_t num = 2;
+  while (count < n) {
+    if (checkPrime(num)) {
+      count++;
     }
-    return num - 1;
+    num++;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
+  if (value <= 1)
     return 2;
-    if (value <= 1)
-        return 2;
-    uint64_t prime = value;
-    bool found = false;
-    while (!found) {
-        prime++;
-        if (chechPrime(prime)) {
-            found = true;
-        }
+  uint64_t prime = value;
+  bool found = false;
+  while (!found) {
+    prime++;
+    if (checkPrime(prime)) {
+      found = true;
     }
-    return prime;
+  }
+  return prime;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    return 2;
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (chechPrime(i)) {
-            sum += i;
-        }
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
     }
-    return sum;
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    return 1;
-    uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound - 1; i++) {
-        if (chechPrime(i) && chechPrime(i + 2)) {
-            count++;
-        }
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i < hbound - 1; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count++;
     }
-    return count;
+  }
+  return count;
+}
